ed67a5876e731cb2b0ea97adc757aa7d328139ee
seveibar <seveibar@gmail.com>
2024-10-13 15:32:51 -0700

    add playwright test workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@playwright/test"
 export default defineConfig({
   // Run your local dev server before starting the tests
   webServer: {
-    command: "npm run start:playwright",
+    command: "npm run start:playwright-server",
     url: "http://127.0.0.1:5177",
     reuseExistingServer: !process.env.CI,
     stdout: "pipe",
