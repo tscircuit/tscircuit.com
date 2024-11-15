0fad79ad8aa504a9008bf30c6182a0ef069a41ce
Mrudul Patil <mrudulsp.18@gmail.com>
2024-11-15 09:21:45 +0530

    removed 3d view from test

--- playwright-tests/preview-page.spec.ts
+++ playwright-tests/preview-page.spec.ts
@@ -11,9 +11,4 @@ test(`preview-snippet Page`, async ({ page }) => {
   // Wait for schematic viewer to load
   await page.waitForTimeout(1000)
   await expect(page).toHaveScreenshot(`preview-snippet-schematic.png`)
-
-  await page.goto("http://127.0.0.1:5177/preview?snippet_id=snippet_5&view=3d")
-  // Wait for 3D viewer to load
-  await page.waitForTimeout(1000)
-  await expect(page).toHaveScreenshot(`preview-snippet-3d.png`)
 })
