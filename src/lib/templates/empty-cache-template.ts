059fe9828bbe4aa51c8369de0f58d4819710f37e
Rishabh Gupta <belikerishabh@gmail.com>
2024-11-20 12:30:39 +0530

    remove test template

--- src/lib/templates/empty-cache-template.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    cacheKey: "",
-    pcbTraces: [],
-  }
-  
--- src/lib/templates/pcb-route-cache-template.ts
+++ src/lib/templates/pcb-route-cache-template.ts
@@ -1,28 +1,4 @@
 export default {
-  cacheKey: "test",
-  pcbTraces: [
-    {
-      type: "pcb_trace",
-      pcb_trace_id: "pcb_trace_0",
-      route: [
-        {
-          route_type: "wire",
-          x: -1.5,
-          y: 0,
-          width: 0.16,
-          layer: "top",
-          start_pcb_port_id: "pcb_port_1",
-        },
-        {
-          route_type: "wire",
-          x: 1.5,
-          y: 0,
-          width: 0.16,
-          layer: "top",
-          end_pcb_port_id: "pcb_port_2",
-        },
-      ],
-      source_trace_id: "source_trace_0",
-    },
-  ],
+  cacheKey: "",
+  pcbTraces: [],
 }
