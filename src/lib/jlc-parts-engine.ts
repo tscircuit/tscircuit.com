d8e6c1bcb4254b127ae7364f8a82fa1b379241c5
Abdoaslam Allawlabi <abdaslam13.2.2001@gmail.com>
2024-11-24 08:39:23 +0200

    removed console logs

--- src/lib/jlc-parts-engine.ts
+++ src/lib/jlc-parts-engine.ts
@@ -4,18 +4,15 @@ import qs from "qs"
 
 const cache = new Map<string, any>()
 const getJlcPartsCached = async (name: any, params: any) => {
-  console.log(params)
   const paramString = qs.stringify({ ...params, json: "true" })
   if (cache.has(paramString)) {
     return cache.get(paramString)
   }
-  // console.log(paramString)
   const response = await fetch(
     `https://jlcsearch.tscircuit.com/${name}/list?${paramString}`,
   )
   const responseJson = await response.json()
   cache.set(paramString, responseJson)
-  // console.log(responseJson)
   return responseJson
 }
 
