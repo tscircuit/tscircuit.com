620e451a662fe1957de542eeb4bd73ec7a956b27
AbseLY <abdaslam13.2.2001@gmail.com>
2024-11-24 08:33:16 +0200

    Implement Capacitor BOM Search

--- src/lib/jlc-parts-engine.ts
+++ src/lib/jlc-parts-engine.ts
@@ -3,16 +3,19 @@ import { AnySourceComponent } from "circuit-json"
 import qs from "qs"
 
 const cache = new Map<string, any>()
-const getJlcPartsCached = async (params: any) => {
+const getJlcPartsCached = async (name: any, params: any) => {
+  console.log(params)
   const paramString = qs.stringify({ ...params, json: "true" })
   if (cache.has(paramString)) {
     return cache.get(paramString)
   }
+  // console.log(paramString)
   const response = await fetch(
-    `https://jlcsearch.tscircuit.com/resistors/list?${paramString}`,
+    `https://jlcsearch.tscircuit.com/${name}/list?${paramString}`,
   )
   const responseJson = await response.json()
   cache.set(paramString, responseJson)
+  // console.log(responseJson)
   return responseJson
 }
 
@@ -22,13 +25,25 @@ export const jlcPartsEngine: PartsEngine = {
     footprinterString,
   }): Promise<SupplierPartNumbers> => {
     if (sourceComponent.ftype === "simple_resistor") {
-      const { resistors } = await getJlcPartsCached({
+      const { resistors } = await getJlcPartsCached("resistors", {
         resistance: sourceComponent.resistance,
         package: footprinterString,
       })
 
       return {
         jlcpcb: resistors.map((r: any) => `C${r.lcsc}`).slice(0, 3),
+      }
+    } else if (sourceComponent.ftype === "simple_capacitor") {
+      if (footprinterString?.includes("cap")) {
+        footprinterString = footprinterString.replace("cap", "")
+      }
+      const { capacitors } = await getJlcPartsCached("capacitors", {
+        capacitance: sourceComponent.capacitance,
+        package: footprinterString,
+      })
+
+      return {
+        jlcpcb: capacitors.map((c: any) => `C${c.lcsc}`).slice(0, 3),
       }
     }
     return {}
