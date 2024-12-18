f1b047c237e14ca36afea5559b60407005ed3984
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2024-12-18 23:41:32 +0530

    add blinking led board template (#343)
    * add blinking led board template
    
    * fix format check workflow error
    
    * remove route hints and add usb0c port

--- src/lib/get-snippet-template.ts
+++ src/lib/get-snippet-template.ts
@@ -3,6 +3,7 @@ import { blankFootprintTemplate } from "@/lib/templates/blank-footprint-template
 import { blankCircuitBoardTemplate } from "@/lib/templates/blank-circuit-board-template"
 import { blank3dModelTemplate } from "@/lib/templates/blank-3d-model-template"
 import { defaultCodeForBlankPage } from "@/lib/defaultCodeForBlankCode"
+import { blinkingLedBoardTemplate } from "@/lib/templates/blinking-led-board-template"
 
 export const getSnippetTemplate = (template: string | undefined) => {
   switch (template) {
@@ -14,6 +15,8 @@ export const getSnippetTemplate = (template: string | undefined) => {
       return blankCircuitBoardTemplate
     case "blank-3d-model":
       return blank3dModelTemplate
+    case "blinking-led-board":
+      return blinkingLedBoardTemplate
     default:
       return { code: defaultCodeForBlankPage, type: "board" }
   }
