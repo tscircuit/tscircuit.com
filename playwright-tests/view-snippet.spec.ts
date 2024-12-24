46b72e9f88bb49bddef2b8ecbf36455eeb735474
seveibar <seveibar@gmail.com>
2024-12-24 11:40:41 -0800

    skip the view snippet test

--- playwright-tests/view-snippet.spec.ts
+++ playwright-tests/view-snippet.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test"
 import { viewports } from "./viewports"
 
 for (const [size, viewport] of Object.entries(viewports)) {
-  test(`view-snippet Page on ${size} screen`, async ({ page }) => {
+  test.skip(`view-snippet Page on ${size} screen`, async ({ page }) => {
     await page.setViewportSize(viewport)
     await page.goto("http://127.0.0.1:5177/testuser/my-test-board")
 
