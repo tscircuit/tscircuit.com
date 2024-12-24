8d1ad1367e77e6c382c80da63d49474d77841ff9
Anshgrover23 <ag5989670@gmail.com>
2024-12-25 01:56:25 +0530

    add waitforLoadState to view-snippet.spec.ts test

--- playwright-tests/view-snippet.spec.ts
+++ playwright-tests/view-snippet.spec.ts
@@ -2,18 +2,21 @@ import { test, expect } from "@playwright/test"
 import { viewports } from "./viewports"
 
 for (const [size, viewport] of Object.entries(viewports)) {
-  test.skip(`view-snippet Page on ${size} screen`, async ({ page }) => {
+  test(`view-snippet Page on ${size} screen`, async ({ page }) => {
     await page.setViewportSize(viewport)
     await page.goto("http://127.0.0.1:5177/testuser/my-test-board")
 
     await page.waitForSelector(".run-button")
+    await page.waitForLoadState("networkidle")
     await expect(page).toHaveScreenshot(`view-snippet-before-${size}.png`)
     await page.click(".run-button")
     await page.waitForTimeout(5000)
+    await page.waitForLoadState("networkidle")
     await expect(page).toHaveScreenshot(`view-snippet-after-${size}.png`)
 
     if (size !== "xs") {
       await page.click('span:has-text("Files")')
+      await page.waitForLoadState("networkidle")
     }
   })
 }
@@ -22,7 +25,8 @@ test("files dialog", async ({ page }) => {
   await page.goto("http://127.0.0.1:5177/testuser/my-test-board")
 
   await page.waitForSelector(".run-button")
-
+  await page.waitForLoadState("networkidle")
   await page.click('span:has-text("Files")')
+  await page.waitForLoadState("networkidle")
   await expect(page).toHaveScreenshot(`view-snippet-files.png`)
 })
