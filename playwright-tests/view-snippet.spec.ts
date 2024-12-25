bc01f744dd0996aa82338da87ae2f710b8be9d4d
Anshgrover23 <ag5989670@gmail.com>
2024-12-25 16:39:58 +0530

    fix: test

--- playwright-tests/view-snippet.spec.ts
+++ playwright-tests/view-snippet.spec.ts
@@ -7,16 +7,14 @@ for (const [size, viewport] of Object.entries(viewports)) {
     await page.goto("http://127.0.0.1:5177/testuser/my-test-board")
 
     await page.waitForSelector(".run-button")
-    await page.waitForLoadState("networkidle")
     await expect(page).toHaveScreenshot(`view-snippet-before-${size}.png`)
     await page.click(".run-button")
-    await page.waitForTimeout(5000)
+
     await page.waitForLoadState("networkidle")
     await expect(page).toHaveScreenshot(`view-snippet-after-${size}.png`)
 
     if (size !== "xs") {
       await page.click('span:has-text("Files")')
-      await page.waitForLoadState("networkidle")
     }
   })
 }
@@ -25,8 +23,7 @@ test("files dialog", async ({ page }) => {
   await page.goto("http://127.0.0.1:5177/testuser/my-test-board")
 
   await page.waitForSelector(".run-button")
-  await page.waitForLoadState("networkidle")
+
   await page.click('span:has-text("Files")')
-  await page.waitForLoadState("networkidle")
   await expect(page).toHaveScreenshot(`view-snippet-files.png`)
 })
