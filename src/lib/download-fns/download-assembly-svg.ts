ee4741b7a525568e987fe668253d1396162c03a0
techmannih <manishchaudharyttt@gmail.com>
2025-01-03 07:42:25 +0530

    add download assembly svg button

---
+++ src/lib/download-fns/download-assembly-svg.ts
@@ -0,0 +1,12 @@
+import { AnyCircuitElement } from "circuit-json"
+import { convertCircuitJsonToAssemblySvg } from "circuit-to-svg"
+import { saveAs } from "file-saver"
+
+export const downloadAssemblySvg = (
+  circuitJson: AnyCircuitElement[],
+  fileName: string,
+) => {
+  const svg = convertCircuitJsonToAssemblySvg(circuitJson)
+  const blob = new Blob([svg], { type: "image/svg" })
+  saveAs(blob, fileName + ".svg")
+}
