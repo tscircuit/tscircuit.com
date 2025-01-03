9ae18abebef1d55462931aa30f99474e0174de0a
seveibar <seveibar@gmail.com>
2025-01-03 12:51:27 -0800

    fix discord link

--- src/components/Header2.tsx
+++ src/components/Header2.tsx
@@ -104,7 +104,7 @@ export const Header2 = () => {
             </a>
             <a
               className="text-sm font-medium hover:underline underline-offset-4"
-              href="https://github.com/tscircuit/tscircuit"
+              href="https://tscircuit.com/join"
             >
               Discord
             </a>
