6e85216ca297542381e4880b16a1e9692ccd9e4f
Ansh Grover <168731971+Anshgrover23@users.noreply.github.com>
2025-01-11 00:11:21 +0530

    fix: if somebody edits, then hits fork, we drop all their changes to the base snippet (#528)
    * fix: If somebody edits, then hits fork, we drop all their changes to the base snippet
    
    * fix: If somebody edits, then hits fork, we drop all their changes to the base snippet

--- src/components/EditorNav.tsx
+++ src/components/EditorNav.tsx
@@ -99,6 +99,7 @@ export default function EditorNav({
 
   const { mutate: forkSnippet, isLoading: isForking } = useForkSnippetMutation({
     snippet: snippet!,
+    currentCode: code,
     onSuccess: (forkedSnippet) => {
       navigate("/editor?snippet_id=" + forkedSnippet.snippet_id)
       setTimeout(() => {
