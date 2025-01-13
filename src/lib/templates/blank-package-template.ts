70541c2bc175c8276a59a6d6f266917ef383394e
techmannih <manishchaudharyttt@gmail.com>
2025-01-14 01:20:55 +0530

    add useMychip hook

--- src/lib/templates/blank-package-template.ts
+++ src/lib/templates/blank-package-template.ts
@@ -7,5 +7,42 @@ export const MyChip = (props: { name: string }) => (
     footprint="soic8"
   />
 )
+  const pinLabels = {
+  "pin1": [
+    "pin1",
+    "RESET"
+  ],
+  "pin2": [
+    "pin2",
+    "XTAL1"
+  ],
+  "pin3": [
+    "pin3",
+    "AVCC"
+  ],
+  "pin4": [
+    "pin4",
+    "VCC"
+  ],
+  "pin5": [
+    "pin5",
+    "GND"
+  ],
+
+  "pin6": [
+    "pin6",
+    "PAD"
+  ],
+  "pin7": [
+    "pin7",
+    "AGND"
+  ],
+  "pin8": [
+    "pin8",
+    "GND3"
+  ]
+} as const
+
+export const useMyChip = createUseComponent(MyChip, pinLabels)
 `.trim(),
 }
