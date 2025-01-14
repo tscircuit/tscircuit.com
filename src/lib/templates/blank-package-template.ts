f0b738566a4b85e5d144c6445d797a7ba8be61fe
techmannih <manishchaudharyttt@gmail.com>
2025-01-14 08:49:27 +0530

    format module code

--- src/lib/templates/blank-package-template.ts
+++ src/lib/templates/blank-package-template.ts
@@ -4,37 +4,17 @@ export const blankPackageTemplate = {
 import { createUseComponent } from "@tscircuit/core"
 
 export const MyChip = (props: { name: string }) => (
-  <chip
-    {...props}
-    pinLabels={pinLabels}
-    footprint="soic8"
-  />
+  <chip {...props} pinLabels={pinLabels} footprint="soic8" />
 )
 const pinLabels = {
-  "pin1": [
-    "pin1"
-  ],
-  "pin2": [
-    "pin2"
-  ],
-  "pin3": [
-    "pin3"
-  ],
-  "pin4": [
-    "pin4"
-  ],
-  "pin5": [
-    "pin5"
-  ],
-  "pin6": [
-    "pin6"
-  ],
-  "pin7": [
-    "pin7"
-  ],
-  "pin8": [
-    "pin8"
-  ]
+  pin1: [],
+  pin2: [],
+  pin3: [],
+  pin4: [],
+  pin5: [],
+  pin6: [],
+  pin7: [],
+  pin8: [],
 } as const
 
 export const useMyChip = createUseComponent(MyChip, pinLabels)
