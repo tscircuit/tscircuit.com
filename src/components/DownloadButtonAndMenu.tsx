30aa6a4a12ceca60508d2ddadbd80b556d03d208
seveibar <seveibar@gmail.com>
2025-01-13 20:09:23 -0800

    download simple route json

--- src/components/DownloadButtonAndMenu.tsx
+++ src/components/DownloadButtonAndMenu.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { toast, useNotImplementedToast } from "@/hooks/use-toast"
 import { downloadCircuitJson } from "@/lib/download-fns/download-circuit-json-fn"
+import { downloadSimpleRouteJson } from "@/lib/download-fns/download-simple-route-json"
 import { downloadDsnFile } from "@/lib/download-fns/download-dsn-file-fn"
 import { downloadFabricationFiles } from "@/lib/download-fns/download-fabrication-files"
 import { downloadSchematicSvg } from "@/lib/download-fns/download-schematic-svg"
@@ -61,7 +62,7 @@ export function DownloadButtonAndMenu({
             }}
           >
             <Download className="mr-1 h-3 w-3" />
-            <span className="flex-grow mr-6">Download Circuit JSON</span>
+            <span className="flex-grow mr-6">Circuit JSON</span>
             <span className="text-[0.6rem] opacity-80 bg-blue-500 text-white font-mono rounded-md px-1 text-center py-0.5 mr-1">
               json
             </span>
@@ -83,7 +84,7 @@ export function DownloadButtonAndMenu({
             }}
           >
             <Download className="mr-1 h-3 w-3" />
-            <span className="flex-grow  mr-6">Download 3D Model</span>
+            <span className="flex-grow  mr-6">3D Model</span>
             <span className="text-[0.6rem] bg-green-500 opacity-80 text-white font-mono rounded-md px-1 text-center py-0.5 mr-1">
               gltf
             </span>
@@ -114,7 +115,7 @@ export function DownloadButtonAndMenu({
             onClick={() => notImplemented("kicad footprint download")}
           >
             <Download className="mr-1 h-3 w-3" />
-            <span className="flex-grow mr-6">Download Footprint</span>
+            <span className="flex-grow mr-6">KiCad Footprint</span>
             <span className="text-[0.6rem] bg-orange-500 opacity-80 text-white font-mono rounded-md px-1 text-center py-0.5 mr-1">
               kicad_mod
             </span>
@@ -129,7 +130,7 @@ export function DownloadButtonAndMenu({
             }}
           >
             <Download className="mr-1 h-3 w-3" />
-            <span className="flex-grow mr-6">Download KiCad Project</span>
+            <span className="flex-grow mr-6">KiCad Project</span>
             <span className="text-[0.6rem] bg-orange-500 opacity-80 text-white font-mono rounded-md px-1 text-center py-0.5 mr-1">
               kicad_*.zip
             </span>
@@ -145,7 +146,7 @@ export function DownloadButtonAndMenu({
             }}
           >
             <Download className="mr-1 h-3 w-3" />
-            <span className="flex-grow mr-6">Download Schematic SVG</span>
+            <span className="flex-grow mr-6">Schematic SVG</span>
             <span className="text-[0.6rem] opacity-80 bg-blue-500 text-white font-mono rounded-md px-1 text-center py-0.5 mr-1">
               svg
             </span>
@@ -157,7 +158,7 @@ export function DownloadButtonAndMenu({
             }}
           >
             <Download className="mr-1 h-3 w-3" />
-            <span className="flex-grow mr-6">Download Assembly SVG</span>
+            <span className="flex-grow mr-6">Assembly SVG</span>
             <span className="text-[0.6rem] opacity-80 bg-blue-500 text-white font-mono rounded-md px-1 text-center py-0.5 mr-1">
               svg
             </span>
@@ -169,7 +170,7 @@ export function DownloadButtonAndMenu({
             }}
           >
             <Download className="mr-1 h-3 w-3" />
-            <span className="flex-grow mr-6">Download DSN file</span>
+            <span className="flex-grow mr-6">Specctra DSN</span>
             <span className="text-[0.6rem] opacity-80 bg-blue-500 text-white font-mono rounded-md px-1 text-center py-0.5 mr-1">
               dsn
             </span>
@@ -184,9 +185,24 @@ export function DownloadButtonAndMenu({
             }}
           >
             <Download className="mr-1 h-3 w-3" />
-            <span className="flex-grow mr-6">Download Readable Netlist</span>
+            <span className="flex-grow mr-6">Readable Netlist</span>
             <span className="text-[0.6rem] opacity-80 bg-blue-500 text-white font-mono rounded-md px-1 text-center py-0.5 mr-1">
               txt
+            </span>
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="text-xs"
+            onSelect={() => {
+              downloadSimpleRouteJson(
+                circuitJson,
+                snippetUnscopedName || "circuit",
+              )
+            }}
+          >
+            <Download className="mr-1 h-3 w-3" />
+            <span className="flex-grow mr-6">Simple Route JSON</span>
+            <span className="text-[0.6rem] opacity-80 bg-blue-500 text-white font-mono rounded-md px-1 text-center py-0.5 mr-1">
+              json
             </span>
           </DropdownMenuItem>
         </DropdownMenuContent>
