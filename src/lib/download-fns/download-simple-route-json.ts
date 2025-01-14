3b275682686cd11b01451a269d4915e915c7e228
Severin Ibarluzea <seveibar@gmail.com>
2025-01-13 20:15:26 -0800

    download simple route json (#560)

---
+++ src/lib/download-fns/download-simple-route-json.ts
@@ -0,0 +1,17 @@
+import { AnyCircuitElement } from "circuit-json"
+import { getSimpleRouteJsonFromCircuitJson } from "@tscircuit/core"
+import { saveAs } from "file-saver"
+
+export const downloadSimpleRouteJson = (
+  circuitJson: AnyCircuitElement[],
+  fileName: string,
+) => {
+  const simpleRouteJson = getSimpleRouteJsonFromCircuitJson({
+    circuitJson: circuitJson as any,
+    minTraceWidth: 0.1,
+  })
+  const blob = new Blob([JSON.stringify(simpleRouteJson, null, 2)], {
+    type: "application/json",
+  })
+  saveAs(blob, fileName + "_routes.json")
+}
