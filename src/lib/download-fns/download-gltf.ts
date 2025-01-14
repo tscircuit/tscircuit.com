b3219bff9b57fcf2e8b2a81bdfe8ba5d7fddeb66
Arnav K <arnavkaushal09@gmail.com>
2025-01-14 10:29:04 +0000

    feat: reduce latency

--- src/lib/download-fns/download-gltf.ts
+++ src/lib/download-fns/download-gltf.ts
@@ -4,14 +4,5 @@ export const downloadGLTFFileFromUrl = async (
   blobUrl: string,
   fileName: string,
 ) => {
-  await fetch(blobUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Failed to fetch Blob. Status: ${response.status}`)
-      }
-      return response.blob()
-    })
-    .then((blob) => {
-      saveAs(blob, `${fileName}.gltf`)
-    })
+  return saveAs(blobUrl, `${fileName}.gltf`)
 }
