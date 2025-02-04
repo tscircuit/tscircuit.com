8166a3c1a0b9b0cfb6528b7367d46468a511d085
Arnav K <arnavkaushal09@gmail.com>
2025-02-04 18:34:29 +0000

    why the duck it is failing

--- scripts/generate_bundle_stats.js
+++ scripts/generate_bundle_stats.js
@@ -83,7 +83,6 @@ function compareSizes(prData, mainData, dependencies) {
     const diff = prSize - mainSize
 
     let percentChange = "N/A"
-
     if (prSize === 0 && mainSize > 0) {
       percentChange = "Removed"
     } else if (mainSize !== 0) {
