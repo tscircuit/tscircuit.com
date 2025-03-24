a439f9bc37bada39724c334a58ef48e4cfde23d4
mustafa <mustafamulla765@gmail.com>
2025-03-24 23:45:33 +0530

    Improved design

--- src/pages/user-profile.tsx
+++ src/pages/user-profile.tsx
@@ -19,6 +19,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { OptimizedImage } from "@/components/OptimizedImage"
+import { useSnippetsBaseApiUrl } from "@/hooks/use-snippets-base-api-url"
+import { TypeBadge } from "@/components/TypeBadge"
 
 export const UserProfilePage = () => {
   const { username } = useParams()
@@ -52,6 +55,8 @@ export const UserProfilePage = () => {
       },
     )
 
+  const baseUrl = useSnippetsBaseApiUrl()
+
   const snippetsToShow =
     activeTab === "starred" ? starredSnippets : userSnippets
   const isLoading =
@@ -68,6 +73,23 @@ export const UserProfilePage = () => {
     e.preventDefault() // Prevent navigation
     setSnippetToDelete(snippet)
     openDeleteDialog()
+  }
+
+  const getRelativeTime = (date: string | number | Date) => {
+    const now = new Date()
+    const diff = Math.floor((now.getTime() - new Date(date).getTime()) / 1000)
+
+    if (diff < 60) return `${diff} seconds ago`
+    const diffMinutes = Math.floor(diff / 60)
+    if (diffMinutes < 60) return `${diffMinutes} minutes ago`
+    const diffHours = Math.floor(diffMinutes / 60)
+    if (diffHours < 24) return `${diffHours} hours ago`
+    const diffDays = Math.floor(diffHours / 24)
+    if (diffDays < 30) return `${diffDays} days ago`
+    const diffMonths = Math.floor(diffDays / 30)
+    if (diffMonths < 12) return `${diffMonths} months ago`
+    const diffYears = Math.floor(diffMonths / 12)
+    return `${diffYears} years ago`
   }
 
   return (
@@ -118,49 +140,69 @@ export const UserProfilePage = () => {
                   key={snippet.snippet_id}
                   href={`/${snippet.owner_name}/${snippet.unscoped_name}`}
                 >
-                  <div className="border p-4 rounded-md hover:shadow-md transition-shadow">
-                    <div className="flex justify-between items-start">
-                      <h3 className="text-md font-semibold">
-                        {snippet.unscoped_name}
-                      </h3>
-                      <div className="flex items-center gap-2">
-                        <div className="flex items-center text-gray-600">
-                          <StarIcon className="w-4 h-4 mr-1" />
-                          <span>{snippet.star_count || 0}</span>
-                        </div>
-                        {snippet.is_private && (
-                          <div className="flex items-center text-gray-600">
-                            <LockClosedIcon className="w-4 h-4 mr-1" />
+                  <div className="border p-4 rounded-md hover:shadow-md transition-shadow flex flex-col gap-4">
+                    <div className="flex items-center gap-4">
+                      <div className="h-16 w-16 rounded-md overflow-hidden">
+                        <OptimizedImage
+                          src={`${baseUrl}/snippets/images/${snippet.owner_name}/${snippet.unscoped_name}/pcb.svg`}
+                          alt={`${snippet.owner_name}'s profile`}
+                          className="object-cover h-full w-full transition-transform duration-300 rotate-45 hover:rotate-0 hover:scale-110 scale-150"
+                        />
+                      </div>
+                      <div className="flex-1">
+                        <div className="">
+                          <div className="flex justify-between items-center">
+                            <h2 className="text-md font-semibold text-gray-900">
+                              {snippet.unscoped_name}
+                            </h2>
+                            <TypeBadge type={snippet.snippet_type} />
                           </div>
-                        )}
-                        {isCurrentUserProfile && activeTab === "all" && (
-                          <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                              <Button
-                                variant="ghost"
-                                size="icon"
-                                className="h-8 w-8"
-                              >
-                                <MoreVertical className="h-4 w-4" />
-                              </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent>
-                              <DropdownMenuItem
-                                className="text-xs text-red-600"
-                                onClick={(e) => handleDeleteClick(e, snippet)}
-                              >
-                                <Trash2 className="mr-2 h-3 w-3" />
-                                Delete Snippet
-                              </DropdownMenuItem>
-                            </DropdownMenuContent>
-                          </DropdownMenu>
-                        )}
+                          <p className="text-sm text-gray-500">
+                            {(snippet.description ?? "").length > 30
+                              ? (snippet.description ?? "").slice(0, 30) + "..."
+                              : (snippet.description ?? "")}
+                          </p>
+                        </div>
+                        <div className="flex justify-between items-center">
+                          <p className="text-sm text-gray-500">
+                            Last Updated: {getRelativeTime(snippet.updated_at)}
+                          </p>
+                          <div className="flex items-center gap-2">
+                            <div className="flex items-center text-gray-600">
+                              <StarIcon className="w-4 h-4 mr-1" />
+                              <span>{snippet.star_count || 0}</span>
+                            </div>
+                            {snippet.is_private && (
+                              <LockClosedIcon className="w-4 h-4 text-gray-600" />
+                            )}
+                            {isCurrentUserProfile && activeTab === "all" && (
+                              <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                  <Button
+                                    variant="ghost"
+                                    size="icon"
+                                    className="h-8 w-8"
+                                  >
+                                    <MoreVertical className="h-4 w-4" />
+                                  </Button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent>
+                                  <DropdownMenuItem
+                                    className="text-xs text-red-600"
+                                    onClick={(e) =>
+                                      handleDeleteClick(e, snippet)
+                                    }
+                                  >
+                                    <Trash2 className="mr-2 h-3 w-3" />
+                                    Delete Snippet
+                                  </DropdownMenuItem>
+                                </DropdownMenuContent>
+                              </DropdownMenu>
+                            )}
+                          </div>
+                        </div>
                       </div>
                     </div>
-                    <p className="text-sm text-gray-500">
-                      Last Updated:{" "}
-                      {new Date(snippet.updated_at).toLocaleString()}
-                    </p>
                   </div>
                 </Link>
               ))}
