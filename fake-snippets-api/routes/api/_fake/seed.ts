5ff29f517fb0107a2cce58b5ae43fd7dfa939a54
Severin Ibarluzea <seveibar@gmail.com>
2025-03-25 14:11:23 -0700

    fake snippets api seed function for RunFrame usage (#753)

---
+++ fake-snippets-api/routes/api/_fake/seed.ts
@@ -0,0 +1,14 @@
+import { withRouteSpec } from "fake-snippets-api/lib/middleware/with-winter-spec"
+import { z } from "zod"
+import { seed } from "fake-snippets-api/lib/db/seed"
+
+export default withRouteSpec({
+  methods: ["POST"],
+  auth: "none",
+  jsonResponse: z.object({ ok: z.boolean() }),
+})(async (req, ctx) => {
+  // Seed the database with initial data
+  seed(ctx.db)
+
+  return ctx.json({ ok: true })
+})
