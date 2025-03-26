97d5ff530f2f5f575b438fa2189cad2fb861a8ee
Mustafa Mulla <97171641+MustafaMulla29@users.noreply.github.com>
2025-03-27 00:03:05 +0530

    author name with package (#757)

--- src/pages/user-profile.tsx
+++ src/pages/user-profile.tsx
@@ -22,7 +22,10 @@ import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { OptimizedImage } from "@/components/OptimizedImage"
 import { useSnippetsBaseApiUrl } from "@/hooks/use-snippets-base-api-url"
 import { SnippetTypeIcon } from "@/components/SnippetTypeIcon"
-import { format } from "timeago.js"
+import TimeAgo from "javascript-time-ago"
+import en from "javascript-time-ago/locale/en"
+
+TimeAgo.addDefaultLocale(en)
 
 export const UserProfilePage = () => {
   const { username } = useParams()
@@ -57,6 +60,7 @@ export const UserProfilePage = () => {
     )
 
   const baseUrl = useSnippetsBaseApiUrl()
+  const timeAgo = new TimeAgo("en-US")
 
   const snippetsToShow =
     activeTab === "starred" ? starredSnippets : userSnippets
@@ -99,7 +103,7 @@ export const UserProfilePage = () => {
         <Tabs defaultValue="all" onValueChange={setActiveTab} className="mb-4">
           <TabsList>
             <TabsTrigger value="all">Snippets</TabsTrigger>
-            <TabsTrigger value="starred">Starred Snippets</TabsTrigger>
+            <TabsTrigger value="starred">Starred Packages</TabsTrigger>
           </TabsList>
         </Tabs>
         <Input
@@ -112,7 +116,7 @@ export const UserProfilePage = () => {
         {isLoading ? (
           <div>
             {activeTab === "starred"
-              ? "Loading starred snippets..."
+              ? "Loading starred Packages..."
               : "Loading user snippets..."}
           </div>
         ) : (
@@ -134,9 +138,19 @@ export const UserProfilePage = () => {
                         />
                       </div>
                       <div className="flex-1 min-w-0">
-                        <div className="flex justify-between items-start mb-[2px]">
-                          <h2 className="text-md font-semibold text-gray-900 truncate">
-                            {snippet.unscoped_name}
+                        <div className="flex justify-between items-start mb-[2px] -mt-[3px]">
+                          <h2 className="text-md font-semibold truncate pr-[30px]">
+                            {activeTab === "starred" && (
+                              <>
+                                <span className="text-gray-700 text-md">
+                                  {snippet.owner_name}
+                                </span>
+                                <span className="mx-1">/</span>
+                              </>
+                            )}
+                            <span className="text-gray-900">
+                              {snippet.unscoped_name}
+                            </span>
                           </h2>
                           <div className="flex items-center gap-2">
                             <SnippetTypeIcon
@@ -155,7 +169,7 @@ export const UserProfilePage = () => {
                                   <Button
                                     variant="ghost"
                                     size="icon"
-                                    className="h-8 w-8"
+                                    className="h-[1.5rem] w-[1.5rem]"
                                   >
                                     <MoreVertical className="h-4 w-4" />
                                   </Button>
@@ -194,7 +208,9 @@ export const UserProfilePage = () => {
                           )}
                           <div className="flex items-center text-xs gap-1 text-gray-500">
                             <PencilIcon height={12} width={12} />
-                            <span>{format(snippet.updated_at)}</span>
+                            <span>
+                              {timeAgo.format(new Date(snippet.updated_at))}
+                            </span>
                           </div>
                         </div>
                       </div>
