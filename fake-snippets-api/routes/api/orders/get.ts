82df072519c415883a1c51d598e343653705a9fa
Rishabh Gupta <belikerishabh@gmail.com>
2025-03-31 18:42:38 +0530

    change the /move_orders_forward to update for all orders present

--- fake-snippets-api/routes/api/orders/get.ts
+++ fake-snippets-api/routes/api/orders/get.ts
@@ -5,27 +5,7 @@ import {
 } from "fake-snippets-api/lib/db/schema"
 import { withRouteSpec } from "fake-snippets-api/lib/middleware/with-winter-spec"
 import { z } from "zod"
-
-export const simulateScenarios = [
-  "are_gerbers_generated",
-  "are_gerbers_uploaded",
-  "is_gerber_analyzed",
-  "are_initial_costs_calculated",
-  "is_pcb_added_to_cart",
-  "is_bom_uploaded",
-  "is_pnp_uploaded",
-  "is_bom_pnp_analyzed",
-  "is_bom_parsing_complete",
-  "are_components_available",
-  "is_patch_map_generated",
-  "is_json_merge_file_created",
-  "is_dfm_result_generated",
-  "are_files_downloaded",
-  "are_product_categories_fetched",
-  "are_final_costs_calculated",
-  "is_json_merge_file_updated",
-  "is_added_to_cart",
-] as const
+import { orderSteps as simulateScenarios } from "fake-snippets-api/utils/order-steps"
 
 const defaultOrderState = {
   are_gerbers_uploaded: false,
