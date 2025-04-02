910a571a7c8cd3b9b5c0d7539da13c3212fc3e29
Rishabh Gupta <belikerishabh@gmail.com>
2025-04-02 18:48:34 +0530

    update the useCurrentPackageId hook

--- src/hooks/use-current-package-id.ts
+++ src/hooks/use-current-package-id.ts
@@ -1,3 +1,46 @@
-import { useCurrentSnippetId } from "./use-current-snippet-id"
+import { useEffect, useState } from "react"
+import { useParams } from "wouter"
+import { usePackageById } from "./use-package-by-package-id"
+import { usePackageByName } from "./use-package-by-package-name"
+import { useUrlParams } from "./use-url-params"
 
-export const useCurrentPackageId = useCurrentSnippetId
+export const useCurrentPackageId = (): {
+  packageId: string | null
+  isLoading: boolean
+  error: (Error & { status: number }) | null
+} => {
+  const urlParams = useUrlParams()
+  const urlPackageId = urlParams.package_id
+  const wouter = useParams()
+  const [packageIdFromUrl, setPackageId] = useState<string | null>(urlPackageId)
+
+  useEffect(() => {
+    if (urlPackageId) {
+      setPackageId(urlPackageId)
+    }
+  }, [urlPackageId])
+
+  const packageName = wouter.author && wouter.packageName 
+    ? `${wouter.author}/${wouter.packageName}`
+    : null
+
+  const {
+    data: packageByName,
+    isLoading: isLoadingPackageByName,
+    error: errorPackageByName,
+  } = usePackageByName(packageName)
+
+  const {
+    data: packageById,
+    isLoading: isLoadingPackageById,
+    error: errorPackageById,
+  } = usePackageById(packageIdFromUrl)
+
+  const packageId = packageIdFromUrl ?? packageByName?.package_id ?? null
+
+  return {
+    packageId,
+    isLoading: isLoadingPackageByName || isLoadingPackageById,
+    error: errorPackageByName || errorPackageById,
+  }
+}
