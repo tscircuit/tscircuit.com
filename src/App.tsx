7b6831ec0bfa11e25fb0cfff372546a583623a07
seveibar <seveibar@gmail.com>
2025-04-01 19:27:55 -0700

    route to view package page

--- src/App.tsx
+++ src/App.tsx
@@ -120,7 +120,11 @@ function App() {
             <Route path="/preview" component={PreviewPage} />
             <Route path="/dev-login" component={DevLoginPage} />
             <Route path="/:username" component={UserProfilePage} />
-            <Route path="/:author/:snippetName" component={ViewSnippetPage} />
+            <Route path="/:author/:snippetName" component={ViewPackagePage} />
+            <Route
+              path="/snippets/:author/:snippetName"
+              component={ViewSnippetPage}
+            />
           </Switch>
         </Suspense>
         <Toaster />
