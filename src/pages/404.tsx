7590d3e0cb8216f686bf8efcd23330d4d30f2e11
Arnav K <arnavkaushal09@gmail.com>
2025-04-16 22:20:13 +0530

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,18 +4,18 @@ import { Header2 } from "@/components/Header2"
 import Footer from "@/components/Footer"
 import { NotFound } from "@/components/NotFound"
 
-export function NotFoundPage(props: { heading?: string }) {
+export function NotFoundPage({heading="Page Not Found"}: { heading?: string }) {
   return (
     <div className="flex min-h-screen flex-col">
       <Helmet>
-        <title>404 - {props.heading} | tscircuit</title>
+        <title>404 - {heading} | tscircuit</title>
         <meta
           name="description"
           content="The page you're looking for doesn't exist."
         />
       </Helmet>
       <Header2 />
-      <NotFound {...props} />
+      <NotFound heading={heading} />
       <Footer />
     </div>
   )
