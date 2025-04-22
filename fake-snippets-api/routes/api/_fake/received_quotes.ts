23135c439f616b77e93c211ef95e60156096d64b
imrishabh18 <belikerishabh@gmail.com>
2025-04-22 15:51:50 +0530

    update the fake endpoint to support mutiple vendors and add fake data

--- fake-snippets-api/routes/api/_fake/received_quotes.ts
+++ fake-snippets-api/routes/api/_fake/received_quotes.ts
@@ -9,58 +9,108 @@ export default withRouteSpec({
     order_quote_id: z.string(),
   }),
   jsonResponse: z.object({
-    order_quote: orderQuoteSchema.optional(),
+    order_quotes: orderQuoteSchema.array().optional(),
     error: z.string().optional(),
   }),
 })(async (req, ctx) => {
   const { order_quote_id } = req.jsonBody
 
   return ctx.json({
-    order_quote: {
-      order_quote_id,
-      account_id: "123",
-      package_release_id: "123",
-      is_completed: true,
-      is_processing: false,
-      vendor_name: "JLCPCB",
-      error: null,
-      has_error: false,
-      created_at: "2025-04-21",
-      updated_at: "2025-04-21",
-      completed_at: "2025-04-21",
-      quoted_components: [
-        {
-          manufacturer_part_number: "123",
-          supplier_part_number: "123",
-          quantity: 1,
-          unit_price: 100,
-          total_price: 100,
-          available: true,
-        },
-        {
-          manufacturer_part_number: "1234",
-          supplier_part_number: "1234",
-          quantity: 1,
-          unit_price: 200,
-          total_price: 200,
-          available: true,
-        },
-      ],
-      bare_pcb_cost: 300,
-      shipping_options: [
-        {
-          carrier: "DHL",
-          service: "Express",
-          cost: 100,
-        },
-        {
-          carrier: "FedEx",
-          service: "Express",
-          cost: 200,
-        },
-      ],
-      // TODO: shipping cost can vary so do the total cost calculation
-      total_cost: 100,
-    },
+    order_quotes: [
+      {
+        order_quote_id,
+        account_id: "123",
+        package_release_id: "123",
+        is_completed: true,
+        is_processing: false,
+        vendor_name: "JLCPCB",
+        error: null,
+        has_error: false,
+        created_at: "2025-04-21",
+        updated_at: "2025-04-21",
+        completed_at: "2025-04-21",
+        quoted_components: [
+          {
+            manufacturer_part_number: "123",
+            supplier_part_number: "123",
+            quantity: 1,
+            unit_price: 100,
+            total_price: 100,
+            available: true,
+          },
+          {
+            manufacturer_part_number: "1234",
+            supplier_part_number: "1234",
+            quantity: 1,
+            unit_price: 200,
+            total_price: 200,
+            available: true,
+          },
+        ],
+        bare_pcb_cost: 300,
+        shipping_options: [
+          {
+            carrier: "DHL",
+            service: "Express",
+            cost: 100,
+          },
+          {
+            carrier: "FedEx",
+            service: "Express",
+            cost: 200,
+          },
+        ],
+        // TODO: shipping cost can vary so do the total cost calculation
+        total_cost: 100,
+        total_cost_after_discount: 50,
+      },
+      {
+        order_quote_id,
+        account_id: "123",
+        package_release_id: "123",
+        is_completed: true,
+        is_processing: false,
+        vendor_name: "DKRed",
+        error: null,
+        has_error: false,
+        created_at: "2025-04-21",
+        updated_at: "2025-04-21",
+        completed_at: "2025-04-21",
+        quoted_components: [
+          {
+            manufacturer_part_number: "123",
+            supplier_part_number: "123",
+            quantity: 1,
+            unit_price: 100,
+            total_price: 100,
+            available: true,
+          },
+          {
+            manufacturer_part_number: "1234",
+            supplier_part_number: "1234",
+            quantity: 1,
+            unit_price: 200,
+            total_price: 200,
+            available: true,
+          },
+        ],
+        bare_pcb_cost: 300,
+        shipping_options: [
+          {
+            carrier: "DHL",
+            service: "Express",
+            cost: 100,
+          },
+          {
+            carrier: "FedEx",
+            service: "Express",
+            cost: 200,
+          },
+        ],
+        // TODO: shipping cost can vary so do the total cost calculation
+        total_cost: 100,
+        total_cost_after_discount: 30,
+      },
+    ],
   })
 })
