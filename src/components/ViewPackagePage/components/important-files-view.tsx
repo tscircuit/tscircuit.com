06ad66423ea80954488f28bd02ecb2813c910de8
imrishabh18 <belikerishabh@gmail.com>
2025-04-24 16:35:09 +0530

    add typography

--- src/components/ViewPackagePage/components/important-files-view.tsx
+++ src/components/ViewPackagePage/components/important-files-view.tsx
@@ -215,8 +215,32 @@ export default function ImportantFilesView({
         {activeTab === "ai" ? (
           renderAiContent()
         ) : activeFilePath && activeFilePath.endsWith(".md") ? (
-          <div className="markdown-content">
-            <Markdown remarkPlugins={[remarkGfm]}>{activeFileContent}</Markdown>
+          <div className="prose dark:prose-invert prose-pre:bg-gray-100 dark:prose-pre:bg-gray-800 prose-code:font-mono markdown-content">
+            <Markdown
+              remarkPlugins={[remarkGfm]}
+              components={{
+                code({ node, className, children, ...props }) {
+                  const isCodeBlock =
+                    className?.includes("language-") ||
+                    /\n/.test(String(children))
+
+                  // Don't use code tags cause of it's backticks not being removed
+                  return isCodeBlock ? (
+                    <div className="bg-gray-100 dark:bg-gray-800 p-4 rounded overflow-auto my-4 w-full">
+                      <span className="text-gray-800 dark:text-gray-200 font-mono whitespace-pre">
+                        {children}
+                      </span>
+                    </div>
+                  ) : (
+                    <span className="bg-gray-100 dark:bg-gray-800 text-gray-800 font-semibold font-mono dark:text-gray-200 px-1 py-0.5 rounded">
+                      {children}
+                    </span>
+                  )
+                },
+              }}
+            >
+              {activeFileContent}
+            </Markdown>
           </div>
         ) : activeFilePath &&
           (activeFilePath.endsWith(".js") ||
