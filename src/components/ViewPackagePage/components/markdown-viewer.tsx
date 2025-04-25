32ce404b53f3b75a8e6565e7b53c09bfac2049ae
Mohan <167054270+mohan-bee@users.noreply.github.com>
2025-04-26 03:22:34 +0530

    fix: readme code syntax highlight (#950)
    * fix: readme code syntax highlight
    
    * chore
    
    * chore
    
    * chore
    
    * chore
    
    * Update fake-snippets-api/routes/api/order_quotes/create.ts
    
    ---------
    
    Co-authored-by: Severin Ibarluzea <seve700@gmail.com>

--- src/components/ViewPackagePage/components/markdown-viewer.tsx
+++ src/components/ViewPackagePage/components/markdown-viewer.tsx
@@ -1,37 +1,54 @@
+import React, { useEffect } from "react"
 import Markdown from "react-markdown"
 import remarkGfm from "remark-gfm"
+
+import { useShikiHighlighter } from "@/hooks/use-shiki-highlighter"
 
 export default function MarkdownViewer({
   markdownContent,
 }: {
   markdownContent: string
 }) {
+  const { highlighter } = useShikiHighlighter()
   return (
-    <div className="prose dark:prose-invert prose-pre:bg-gray-100 dark:prose-pre:bg-gray-800 prose-code:font-mono markdown-content">
-      <Markdown
-        remarkPlugins={[remarkGfm]}
-        components={{
-          code({ node, className, children, ...props }) {
-            const isCodeBlock =
-              className?.includes("language-") || /\n/.test(String(children))
-
-            // Don't use code tags cause of it's backticks not being removed
-            return isCodeBlock ? (
-              <div className="bg-gray-100 dark:bg-gray-800 rounded overflow-auto w-full">
-                <span className="text-gray-800 dark:text-gray-200 font-mono whitespace-pre">
+    <div className="markdown-code">
+      <div className="prose dark:prose-invert prose-pre:py-0 prose-pre:px-6 prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-code:font-mono markdown-content">
+        <Markdown
+          remarkPlugins={[remarkGfm]}
+          components={{
+            code({ node, className, children, ...props }) {
+              const isCodeBlock =
+                className?.includes("language-") || /\n/.test(String(children))
+              const dom = document.createElement("div")
+              if (highlighter) {
+                dom.innerHTML = highlighter.codeToHtml(
+                  children?.toString() || "",
+                  {
+                    lang: "typescript",
+                    themes: {
+                      light: "github-light",
+                      dark: "github-dark",
+                    },
+                  },
+                )
+              }
+              // Don’t use <code> tags (they leave backticks intact)
+              return isCodeBlock ? (
+                <div
+                  dangerouslySetInnerHTML={{ __html: dom.innerHTML }}
+                  className="border rounded-lg"
+                ></div>
+              ) : (
+                <span className="bg-gray-100 dark:bg-gray-800 text-gray-800 font-semibold font-mono dark:text-gray-200 px-1 py-0.5 rounded">
                   {children}
                 </span>
-              </div>
-            ) : (
-              <span className="bg-gray-100 dark:bg-gray-800 text-gray-800 font-semibold font-mono dark:text-gray-200 px-1 py-0.5 rounded">
-                {children}
-              </span>
-            )
-          },
-        }}
-      >
-        {markdownContent}
-      </Markdown>
+              )
+            },
+          }}
+        >
+          {markdownContent}
+        </Markdown>
+      </div>
     </div>
   )
 }
