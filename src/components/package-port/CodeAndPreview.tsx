109c55a18bc3f3964b956427dbf6c2070351d4a9
Arnav K <arnavkaushal09@gmail.com>
2025-04-26 12:21:33 +0530

    chore

--- src/components/package-port/CodeAndPreview.tsx
+++ src/components/package-port/CodeAndPreview.tsx
@@ -35,40 +35,8 @@ export interface PackageFile {
 const DEFAULT_MANUAL_EDITS = ""
 const DEFAULT_CODE = `
 export default () => (
-  <board
-    width="10mm"
-    height="10mm"
-    manualEdits={{
-      pcb_placements: [
-        {
-          selector: "C1",
-          center: {
-            x: -2.56989247311828,
-            y: 3.7849462365591395,
-          },
-          relative_to: "group_center",
-        },
-      ],
-            schematic_placements: [
-        {
-          selector: "C1",
-          center: {
-            x: -2.56989247311828,
-            y: 3.7849462365591395,
-          },
-          relative_to: "group_center",
-        },
-      ],
-    }}
-  >
-    <resistor resistance="1k" footprint="0402" name="R1" schX={3} pcbX={3} />
-    <capacitor
-      capacitance="1000pF"
-      footprint="0402"
-      name="C1"
-      schX={-3}
-      pcbX={-6}
-    />
+  <board width="10mm" height="10mm">
+    {/* write your code here! */}
   </board>
 )
 `.trim()
