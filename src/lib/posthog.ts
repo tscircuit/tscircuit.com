a27cd3b3c6beae62b290fd8bc964f6b4cb767ff4
imrishabh18 <belikerishabh@gmail.com>
2025-04-26 21:17:30 +0530

    Stop sending events to posthog during development

--- src/lib/posthog.ts
+++ src/lib/posthog.ts
@@ -1,10 +1,15 @@
 import posthog from "posthog-js"
 
-if (!posthog.__loaded) {
-  posthog.init("phc_htd8AQjSfVEsFCLQMAiUooG4Q0DKBCjqYuQglc9V3Wo", {
-    api_host: "https://postpig.tscircuit.com",
-    person_profiles: "always",
-  })
+if (
+  !window.location.hostname.includes("localhost") &&
+  !window.location.hostname.includes("127.0.0.1")
+) {
+  if (!posthog.__loaded) {
+    posthog.init("phc_htd8AQjSfVEsFCLQMAiUooG4Q0DKBCjqYuQglc9V3Wo", {
+      api_host: "https://postpig.tscircuit.com",
+      person_profiles: "always",
+    })
+  }
 }
 
 export { posthog }
