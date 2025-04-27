49f625e45a3021196c2c2376efc336a54e70f100
Arnav K <arnavkaushal09@gmail.com>
2025-04-27 21:54:23 +0530

    fix(CodeEditor): ensure consistent path handling in fsMap (#978)

--- src/components/package-port/CodeEditor.tsx
+++ src/components/package-port/CodeEditor.tsx
@@ -122,7 +122,7 @@ export const CodeEditor = ({
 
     const fsMap = new Map<string, string>()
     files.forEach(({ path, content }) => {
-      fsMap.set(path, content)
+      fsMap.set(`${path.startsWith("/") ? "" : "/"}${path}`, content)
     })
     ;(window as any).__DEBUG_CODE_EDITOR_FS_MAP = fsMap
 
@@ -185,7 +185,7 @@ export const CodeEditor = ({
       delegate: {
         started: () => {
           const manualEditsTypeDeclaration = `
-				  declare module "*.json" {
+				  declare module "manual-edits.json" {
 				  const value: {
 					  pcb_placements?: any[],
             schematic_placements?: any[],
