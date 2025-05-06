aaf594a5b964a6a2865fd36b17d175b669cad934
Arnav K <arnavkaushal09@gmail.com>
2025-05-06 23:05:53 +0530

    Update checkIfManualEditsImported.ts

--- src/lib/utils/checkIfManualEditsImported.ts
+++ src/lib/utils/checkIfManualEditsImported.ts
@@ -1,9 +1,8 @@
 export const checkIfManualEditsImported = (
   files: Record<string, string>,
-  file: string,
+  file: string = "index.tsx",
 ) => {
   if (!files[file]) return false
-  if (!file.endsWith(".ts") && !file.endsWith(".tsx")) return false
   const importRegex =
     /import\s+(?:\*\s+as\s+)?([a-zA-Z_$][\w$]*)\s+from\s+["'](?:\.\/)?manual-edits\.json["'];?/
   const hasManualEditsImported = importRegex.test(files[file])
