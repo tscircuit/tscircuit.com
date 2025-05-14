c7510d546f828acf41c992ab9edfd48b6d775953
seveibar <seveibar@gmail.com>
2025-05-13 20:29:55 -0700

    break up shiki

--- src/components/ViewPackagePage/components/ShikiCodeViewer.tsx
+++ src/components/ViewPackagePage/components/ShikiCodeViewer.tsx
@@ -1,43 +1,20 @@
+import { useShikiHighlighter } from "@/hooks/use-shiki-highlighter"
 import { useEffect, useMemo } from "react"
 import { useQuery } from "react-query"
-import { createHighlighter, Highlighter } from "shiki"
-
-let globalHighlighter$: any
-let globalHighlighter: Highlighter
-
-const fileExtensionsToLanguages = {
-  ts: "typescript",
-  tsx: "typescript",
-  js: "javascript",
-  jsx: "javascript",
-}
 
 export const ShikiCodeViewer = ({
   code,
   filePath,
 }: { code: string; filePath: string }) => {
-  useEffect(() => {
-    async function setupHighlighter() {
-      if (globalHighlighter$) return
-      globalHighlighter$ = await createHighlighter({
-        langs: ["typescript"],
-        themes: ["vitesse-light"],
-      })
-      globalHighlighter = await globalHighlighter$
-    }
-    setupHighlighter()
-  }, [])
+  const { highlighter } = useShikiHighlighter()
 
   const html = useMemo(
     () =>
-      globalHighlighter?.codeToHtml(code, {
-        lang:
-          fileExtensionsToLanguages[
-            filePath.split(".").pop() as keyof typeof fileExtensionsToLanguages
-          ] || "typescript",
+      highlighter?.codeToHtml(code, {
+        lang: "tsx",
         theme: "vitesse-light",
       }),
-    [filePath, code, globalHighlighter],
+    [filePath, code, highlighter],
   )
 
   if (!html) {
