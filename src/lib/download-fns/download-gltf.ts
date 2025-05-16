3fada1b7a958ef587e7f09f7ed666a1091e9429c
Arnav K <arnavkaushal09@gmail.com>
2025-05-16 19:31:25 +0530

    refactor(download-gltf): fix model not downlaoding and improve error message for 3D model download

--- src/lib/download-fns/download-gltf.ts
+++ src/lib/download-fns/download-gltf.ts
@@ -1,19 +1,12 @@
-import { AnyCircuitElement } from "circuit-json"
-import { convertCircuitJsonToAssemblySvg } from "circuit-to-svg"
 import { GLTFExporter, type GLTFExporterOptions } from "three-stdlib"
 import { saveAs } from "file-saver"
 import * as THREE from "three"
 
-export const downloadGltf = async (
-  circuitJson: AnyCircuitElement[],
-  fileName: string,
-) => {
-  const threeJsObject = window.TSCIRCUIT_3D_OBJECT_REF
-    ?.current as THREE.Object3D
-
+export const downloadGltf = async (fileName: string) => {
+  const threeJsObject = window.TSCIRCUIT_3D_OBJECT_REF as THREE.Object3D
   if (!threeJsObject) {
     throw new Error(
-      "To download the 3D model, please open the 3D view first and run the snippet",
+      "To download the 3D model, please open the 3D view first and run the package",
     )
   }
 
