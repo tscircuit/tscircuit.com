35134aaa975b4c6af4a4f756af220810360967d3
Arnav K <arnavkaushal09@gmail.com>
2025-05-28 09:44:27 +0530

    feat: restrict access to private packages based on user session (#1170)
    * feat: restrict access to private packages based on user session
    
    * feat: enhance private package access control with NotFound page redirection
    
    * fix
    
    * wtf
    
    * fkd
    
    * final try
    
    * chore

--- src/pages/package-editor.tsx
+++ src/pages/package-editor.tsx
@@ -10,13 +10,14 @@ import { useGetFsMapHashForPackage } from "@/hooks/use-get-fsmap-hash-for-packag
 
 export const EditorPage = () => {
   const { packageId } = useCurrentPackageId()
-  const { data: pkg, isLoading, error } = usePackage(packageId)
+  const { data: pkg, error } = usePackage(packageId)
   const fsMapHash = useGetFsMapHashForPackage(
     pkg?.latest_package_release_id ?? "",
   )
   const uuid4RegExp = new RegExp(
     /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/,
   )
+
   return (
     <div className="overflow-x-hidden">
       <Helmet>
