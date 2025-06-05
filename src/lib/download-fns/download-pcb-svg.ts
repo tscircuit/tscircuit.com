fad641006ae95f3e99f8fd59cc1bd0122f1b8321
Severin Ibarluzea <seveibar@gmail.com>
2025-06-05 16:13:28 -0700

    Add PCB download options dialog (#1207)
    * Add configurable PCB download dialog
    
    * Fix PCB download select dropdown z-index
    
    * Pass download options to pcb converter

--- src/lib/download-fns/download-pcb-svg.ts
+++ src/lib/download-fns/download-pcb-svg.ts
@@ -2,11 +2,34 @@ import { AnyCircuitElement } from "circuit-json"
 import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 import { saveAs } from "file-saver"
 
+export interface DownloadPcbSvgOptions {
+  layer?: "all" | "top" | "bottom"
+  drawPaddingOutsideBoard?: boolean
+  backgroundColor?: string
+  matchAspectRatio?: boolean
+}
+
 export const downloadPcbSvg = (
   circuitJson: AnyCircuitElement[],
   fileName: string,
+  options: DownloadPcbSvgOptions = {},
 ) => {
-  const svg = convertCircuitJsonToPcbSvg(circuitJson)
-const blob = new Blob([svg], { type: "image/svg+xml" })
+  const convertOptions: any = {}
+  if (options.layer && options.layer !== "all") {
+    convertOptions.layer = options.layer
+  }
+  if (options.matchAspectRatio) {
+    convertOptions.matchBoardAspectRatio = true
+  }
+  if (typeof options.drawPaddingOutsideBoard === "boolean") {
+    convertOptions.drawPaddingOutsideBoard = options.drawPaddingOutsideBoard
+  }
+  if (options.backgroundColor) {
+    convertOptions.backgroundColor = options.backgroundColor
+  }
+
+  const svg = convertCircuitJsonToPcbSvg(circuitJson, convertOptions)
+
+  const blob = new Blob([svg], { type: "image/svg+xml" })
   saveAs(blob, fileName + ".svg")
 }
