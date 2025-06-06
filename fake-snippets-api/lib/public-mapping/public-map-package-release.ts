1eb565c48110bb35cfa187d3eda6dfda6b5bd2a6
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-06-06 13:47:15 +0530

    Modify /get endpoint to have include_logs as params, remove the `eval` dependency (#1208)

--- fake-snippets-api/lib/public-mapping/public-map-package-release.ts
+++ fake-snippets-api/lib/public-mapping/public-map-package-release.ts
@@ -2,6 +2,11 @@ import * as ZT from "fake-snippets-api/lib/db/schema"
 
 export const publicMapPackageRelease = (
   internal_package_release: ZT.PackageRelease,
+  options: {
+    include_logs?: boolean
+  } = {
+    include_logs: false,
+  },
 ): ZT.PackageRelease => {
   return {
     ...internal_package_release,
@@ -9,5 +14,11 @@ export const publicMapPackageRelease = (
     circuit_json_build_error_last_updated_at:
       internal_package_release.circuit_json_build_error_last_updated_at,
     circuit_json_build_error: internal_package_release.circuit_json_build_error,
+    transpilation_logs: options.include_logs
+      ? internal_package_release.transpilation_logs
+      : [],
+    circuit_json_build_logs: options.include_logs
+      ? internal_package_release.circuit_json_build_logs
+      : [],
   }
 }
