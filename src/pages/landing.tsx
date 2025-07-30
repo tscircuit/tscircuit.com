312ef715f4424ada6010a273a705b07966902109
Severin Ibarluzea <seve700@gmail.com>
2025-07-30 11:08:05 -0700

    Update online example link on landing page

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -73,7 +73,7 @@ export function LandingPage() {
                       </Button>
                     </a>
                     <PrefetchPageLink
-                      href="/seveibar/usb-c-flashlight#3d"
+                      href="/seveibar/led-water-accelerometer#3d"
                       className="w-[70vw] min-[500px]:w-auto"
                     >
                       <Button
