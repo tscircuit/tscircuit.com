9ba6a293e658555ed44bf25e62f2c8a46fd40e75
Arnav K <arnavkaushal09@gmail.com>
2025-09-27 13:37:25 +0530

    Refactor caching strategy in useOptimizedPackageFilesLoader and useUpdatePackageFilesMutation to set cacheTime to 0 and invalidate queries for package files on save

--- src/hooks/useUpdatePackageFilesMutation.ts
+++ src/hooks/useUpdatePackageFilesMutation.ts
@@ -96,7 +96,8 @@ export function useUpdatePackageFilesMutation({
           title: `Package's ${updatedFilesCount} files saved`,
           description: "Your changes have been saved successfully.",
         })
-        console.log("INVALIDATING")
+        queryClient.invalidateQueries({ queryKey: ["packageFile"] })
+        queryClient.invalidateQueries({ queryKey: ["priorityPackageFile"] })
         queryClient.invalidateQueries({
           predicate: (q) => {
             const key = q.queryKey as any
