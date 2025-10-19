d89b3b8fe95c5e6650cdcb5d414a4bb04396c360
Arnav K <arnavkaushal09@gmail.com>
2025-10-20 04:24:08 +0530

    Revert "f"
    This reverts commit 0e8fe78a6441a97fea666712e568f05c06486910.

--- fake-snippets-api/lib/db/db-client.ts
+++ fake-snippets-api/lib/db/db-client.ts
@@ -32,9 +32,6 @@ import {
 } from "./schema.ts"
 import { seed as seedFn } from "./seed"
 import { generateFsSha } from "../package_file/generate-fs-sha"
-import { useApiBaseUrl } from "@/hooks/use-packages-base-api-url.ts"
-
-const apiUrl = useApiBaseUrl()
 
 export const createDatabase = ({ seed }: { seed?: boolean } = {}) => {
   const db = hoist(createStore(initializer))
@@ -1258,13 +1255,13 @@ const initializer = combine(databaseSchema.parse({}), (set, get) => ({
       github_repo_full_name: null,
       latest_pcb_preview_image_url:
         _package.latest_pcb_preview_image_url ??
-        `${apiUrl}/packages/images/${_package.name}`,
+        `/api/packages/images/${_package.name}`,
       latest_cad_preview_image_url:
         _package.latest_cad_preview_image_url ??
-        `${apiUrl}/packages/images/${_package.name}`,
+        `/api/packages/images/${_package.name}`,
       latest_sch_preview_image_url:
         _package.latest_sch_preview_image_url ??
-        `${apiUrl}/packages/images/${_package.name}`,
+        `/api/packages/images/${_package.name}`,
       ..._package,
     }
     set((state) => ({
