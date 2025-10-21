b783d9dfd73a8cd2394a175552c72400405160ab
Arnav K <arnavkaushal09@gmail.com>
2025-10-22 00:52:47 +0530

    Discard changes to src/pages/package-editor.tsx

--- src/pages/package-editor.tsx
+++ src/pages/package-editor.tsx
@@ -4,7 +4,6 @@ import { Helmet } from "react-helmet-async"
 import { NotFound } from "@/components/NotFound"
 import { ErrorOutline } from "@/components/ErrorOutline"
 import { useCurrentPackageInfo } from "@/hooks/use-current-package-info"
-import { getPackagePreviewImageUrl } from "@/lib/utils/getPackagePreviewImageUrl"
 
 export const EditorPage = () => {
   const { packageInfo: pkg, error } = useCurrentPackageInfo()
@@ -25,12 +24,12 @@ export const EditorPage = () => {
             />
             <meta
               property="og:image"
-              content={getPackagePreviewImageUrl(pkg)}
+              content={`https://api.tscircuit.com/packages/images/${pkg.owner_github_username}/${pkg.unscoped_name}/pcb.png?fs_sha=${pkg.latest_package_release_fs_sha}`}
             />
             <meta name="twitter:card" content="summary_large_image" />
             <meta
               name="twitter:image"
-              content={getPackagePreviewImageUrl(pkg)}
+              content={`https://api.tscircuit.com/packages/images/${pkg.owner_github_username}/${pkg.unscoped_name}/pcb.png?fs_sha=${pkg.latest_package_release_fs_sha}`}
             />
           </>
         )}
