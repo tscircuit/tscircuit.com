e9c2fbdc3b1b4dcc22857cfe382e86383e669eea
Arnav K <arnavkaushal09@gmail.com>
2025-10-31 00:14:57 +0530

    change toast pos

--- src/ContextProviders.tsx
+++ src/ContextProviders.tsx
@@ -87,7 +87,7 @@ export const ContextProviders = ({ children }: any) => {
       <HelmetProvider>
         <PostHogIdentifier />
         {children}
-        <Toaster position="bottom-right" />
+        <Toaster position="bottom-left" />
       </HelmetProvider>
     </QueryClientProvider>
   )
