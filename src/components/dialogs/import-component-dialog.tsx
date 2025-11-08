6b0fc6d8efb37aae25e02ee887b46d09b9313f98
Arnav K <arnavkaushal09@gmail.com>
2025-11-08 23:30:53 +0530

    feat: add session check in import component  (#1964)

--- src/components/dialogs/import-component-dialog.tsx
+++ src/components/dialogs/import-component-dialog.tsx
@@ -1,9 +1,12 @@
+import { useEffect, useRef } from "react"
 import { useApiBaseUrl } from "@/hooks/use-packages-base-api-url"
 import { createUseDialog } from "./create-use-dialog"
 import {
   ImportComponentDialog2 as RunframeImportComponentDialog,
   type ImportComponentDialog2Props,
 } from "@tscircuit/runframe/runner"
+import { useGlobalStore } from "@/hooks/use-global-store"
+import { useToast } from "@/hooks/use-toast"
 
 export type ImportComponentDialogProps = {
   open: boolean
@@ -22,7 +25,22 @@ export const ImportComponentDialog = ({
   onOpenChange,
   ...rest
 }: ImportComponentDialogProps) => {
+  const session = useGlobalStore((s) => s.session)
   const apiBaseUrl = useApiBaseUrl()
+  const { toastLibrary } = useToast()
+  const prevOpenRef = useRef(false)
+
+  useEffect(() => {
+    if (open && !prevOpenRef.current && !session) {
+      toastLibrary.error("Please sign in to import components")
+      onOpenChange(false)
+    }
+    prevOpenRef.current = open
+  }, [open, session, onOpenChange])
+
+  if (!session) {
+    return null
+  }
 
   return (
     <RunframeImportComponentDialog
