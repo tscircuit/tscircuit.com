a78dd0a1e1cf6ecf2b3ce9001eaef8fc53aace1d
Arnav K <arnavkaushal09@gmail.com>
2025-11-18 14:25:10 +0530

    Do not lower case user input (#2039)

--- src/lib/utils/normalizeName.ts
+++ src/lib/utils/normalizeName.ts
@@ -1,7 +1,6 @@
 export const normalizeName = (input: string): string => {
   return input
     .trim() // remove whitespace
-    .toLowerCase() // convert to lowercase
     .normalize("NFD") // decompose unicode characters
     .replace(/[\u0300-\u036f]/g, "") // remove diacritics
     .replace(/[^a-z0-9_-]/g, "-") // replace invalid chars with dash
