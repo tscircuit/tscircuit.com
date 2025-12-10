418b6f660a9f37001a2f36d2d31f24a43c165a0b
Arnav K <arnavkaushal09@gmail.com>
2025-12-11 04:06:11 +0530

    f

--- src/components/package-port/EditorNav.tsx
+++ src/components/package-port/EditorNav.tsx
@@ -55,6 +55,7 @@ import {
 } from "@/components/ui/tooltip"
 
 export default function EditorNav({
+  isPackageFetched,
   circuitJson,
   pkg,
   code,
@@ -70,6 +71,7 @@ export default function EditorNav({
   packageFilesMeta,
 }: {
   pkg?: Package | null
+  isPackageFetched?: boolean
   circuitJson?: AnyCircuitElement[] | null
   code: string
   fsMap: Record<string, string>
@@ -351,29 +353,27 @@ export default function EditorNav({
               Not logged in, can't save
             </div>
           )}
-          {(canManagePackage || (isLoggedIn && pkg)) && (
-            <Button
-              variant="outline"
-              size="sm"
-              className={"ml-1 h-6 px-2 text-xs save-button"}
-              disabled={
-                canManagePackage && pkg ? !hasUnsavedChanges : !isLoggedIn
-              }
-              onClick={canManagePackage ? onSave : () => forkSnippet()}
-            >
-              {canManagePackage ? (
-                <>
-                  <Save className="mr-1 h-3 w-3" />
-                  Save
-                </>
-              ) : (
-                <>
-                  <GitFork className="mr-1 h-3 w-3" />
-                  Fork
-                </>
-              )}
-            </Button>
-          )}
+          <Button
+            variant="outline"
+            size="sm"
+            className={"ml-1 h-6 px-2 text-xs save-button"}
+            disabled={
+              canManagePackage && pkg ? !hasUnsavedChanges : !isLoggedIn
+            }
+            onClick={canManagePackage ? onSave : () => forkSnippet()}
+          >
+            {canManagePackage ? (
+              <>
+                <Save className="mr-1 h-3 w-3" />
+                Save
+              </>
+            ) : (
+              <>
+                <GitFork className="mr-1 h-3 w-3" />
+                Fork
+              </>
+            )}
+          </Button>
           {isSaving && (
             <div className="animate-fadeIn bg-blue-100 select-none text-blue-800 text-xs font-medium px-2.5 py-0.5 rounded flex items-center">
               <Loader2 className="animate-spin h-3 w-3 mr-2 text-blue-600" />
