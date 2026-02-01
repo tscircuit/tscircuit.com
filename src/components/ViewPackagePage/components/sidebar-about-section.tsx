ed56adc1b24a3aa3b2ef3b10b15df138272353c9
Arnav K <arnavkaushal09@gmail.com>
2026-02-01 16:54:04 +0530

    f

--- src/components/ViewPackagePage/components/sidebar-about-section.tsx
+++ src/components/ViewPackagePage/components/sidebar-about-section.tsx
@@ -213,7 +213,10 @@ export default function SidebarAboutSection({
           rel="noopener noreferrer"
           className="text-blue-600 font-medium dark:text-[#58a6ff] hover:underline text-sm flex items-center mb-4 max-w-full overflow-hidden"
         >
-          <Boxes className="h-4 w-4 min-w-[16px] mr-1 flex-shrink-0" />
+          <Boxes
+            strokeWidth={1.5}
+            className="h-4 w-4 min-w-[16px] mr-1 flex-shrink-0"
+          />
           <span className="truncate">KiCad PCM Repository</span>
         </a>
       )}
