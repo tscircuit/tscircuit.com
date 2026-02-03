7a1799a04cd3fe8e0970f04fdab0add36d0f856e
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2026-02-03 19:45:59 +0530

    fix: template for the sample boards should not use the useComponent hook (#2638)
    * fix: template for the sample boards should not use the useComponent hook
    
    * remove cloud autorouting

--- src/lib/templates/usb-c-led-flashlight-template.ts
+++ src/lib/templates/usb-c-led-flashlight-template.ts
@@ -1,20 +1,18 @@
 export const usbCLedFlashlightTemplate = {
   type: "board",
   code: `
-import { useRedLed } from "@tsci/seveibar.red-led"
+import { RedLed } from "@tsci/seveibar.red-led"
 import { PushButton } from "@tsci/seveibar.push-button"
-import { useUsbC } from "@tsci/seveibar.smd-usb-c"
+import { SmdUsbC } from "@tsci/seveibar.smd-usb-c"
 
 export default () => {
-  const USBC = useUsbC("USBC")
-  const Led = useRedLed("LED")
   return (
-    <board width="12mm" height="30mm" schAutoLayoutEnabled autorouter="auto-cloud">
-      <USBC GND1="net.GND" GND2="net.GND" pcbY={-10} VBUS1="net.VBUS" VBUS2="net.VBUS" />
-      <Led neg="net.GND" pcbY={12} />
+    <board width="12mm" height="30mm" schAutoLayoutEnabled>
+      <SmdUsbC GND1="net.GND" GND2="net.GND" pcbY={-10} VBUS1="net.VBUS" VBUS2="net.VBUS" />
+      <RedLed name="Led" neg="net.GND" pcbY={12} />
       <PushButton name="SW1" pcbY={0} pin2=".R1 > .pos" pin3="net.VBUS" />
       <resistor name="R1" footprint="0603" resistance="1k" pcbY={7} />
-      <trace from=".R1 > .neg" to={Led.pos} />
+      <trace from=".R1 > .neg" to={"Led.pos"} />
     </board>
   )
 }`.trim(),
