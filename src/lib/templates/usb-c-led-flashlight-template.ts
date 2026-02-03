39063d4d6dd58fb5ed966071c002d7e1b2eb13ab
imrishabh18 <belikerishabh@gmail.com>
2026-02-03 19:42:05 +0530

    remove cloud autorouting

--- src/lib/templates/usb-c-led-flashlight-template.ts
+++ src/lib/templates/usb-c-led-flashlight-template.ts
@@ -7,12 +7,12 @@ import { SmdUsbC } from "@tsci/seveibar.smd-usb-c"
 
 export default () => {
   return (
-    <board width="12mm" height="30mm" schAutoLayoutEnabled autorouter="auto-cloud">
+    <board width="12mm" height="30mm" schAutoLayoutEnabled>
       <SmdUsbC GND1="net.GND" GND2="net.GND" pcbY={-10} VBUS1="net.VBUS" VBUS2="net.VBUS" />
-      <RedLed neg="net.GND" pcbY={12} />
+      <RedLed name="Led" neg="net.GND" pcbY={12} />
       <PushButton name="SW1" pcbY={0} pin2=".R1 > .pos" pin3="net.VBUS" />
       <resistor name="R1" footprint="0603" resistance="1k" pcbY={7} />
-      <trace from=".R1 > .neg" to={Led.pos} />
+      <trace from=".R1 > .neg" to={"Led.pos"} />
     </board>
   )
 }`.trim(),
