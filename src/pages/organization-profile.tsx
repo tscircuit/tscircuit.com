c07e05b47850703047faffe5920b3f33a85ad9e6
Arnav K <arnavkaushal09@gmail.com>
2026-03-03 23:22:28 +0530

    Update organization-profile.tsx

--- src/pages/organization-profile.tsx
+++ src/pages/organization-profile.tsx
@@ -142,7 +142,7 @@ export const OrganizationProfilePageContent = ({
                     ))}
                   </div>
                 ) : (
-                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                     {filteredPackages?.length !== 0 ? (
                       filteredPackages?.map((pkg) => (
                         <PackageCard
